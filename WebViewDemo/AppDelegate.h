b71ee02ed8ee4f6c7c2f367134e1e812c97404b0
Satish Chauhan <satishchauhan80@gmail.com>
2014-12-10 19:04:45 +0530

    Initial Commit

---
+++ WebViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WebViewDemo
+//
+//  Created by Satish Chauhan on 10/12/14.
+//  Copyright (c) 2014 Vinsol. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WebViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WebViewDemo
+//
+//  Created by Satish Chauhan on 10/12/14.
+//  Copyright (c) 2014 Vinsol. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
